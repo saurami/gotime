b3d93d985b84779cc85b191663ffac7332cf1b7e
Saurabh Mishra <saurabhm@proton.me>
2022-07-09 19:13:13 -0700

    sub-tests and table-driven tests

--- assemblyline/carassembly_test.go
+++ assemblyline/carassembly_test.go
@@ -21,3 +21,22 @@ func TestWorkingCars(t *testing.T) {
 		}
 	})
 }
+
+func TestProductionCost(t *testing.T) {
+	var tests = []struct {
+		description string
+		got uint32
+		want uint32
+	}{
+		{"individual cars only", calculateProductionCost(6),  60000},
+		{"two groups of cars",   calculateProductionCost(21), 200000},
+		{"three groups of cars", calculateProductionCost(37), 355000},
+	}
+
+	for _, test := range tests {
+		t.Logf("testing for %s", test.description)
+		if test.got != test.want {
+			t.Errorf("Issue calculating production cost; got %v, watn %v", test.got, test.want)
+		}
+	}
+}
