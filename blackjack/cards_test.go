a4ef0c1a44f6f20a2a7ac4ee737bc600ca6183b0
Saurabh Mishra <saurabhm@proton.me>
2022-07-23 22:59:54 -0700

    more test cases to cover all switch statements

--- blackjack/cards_test.go
+++ blackjack/cards_test.go
@@ -11,6 +11,14 @@ func TestCardValue(t *testing.T) {
 		{"ace", parseCard("ace"), 11},
 		{"two", parseCard("two"), 2},
 		{"three", parseCard("three"), 3},
+		{"four", parseCard("four"), 4},
+		{"five", parseCard("five"), 5},
+		{"six", parseCard("six"), 6},
+		{"seven", parseCard("seven"), 7},
+		{"eight", parseCard("eight"), 8},
+		{"nine", parseCard("nine"), 9},
+		{"king", parseCard("king"), 10},
+		{"demo", parseCard("demo"), 0},
 	}
 
 	for _, test := range tests {
